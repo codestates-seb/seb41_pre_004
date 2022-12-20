90647d427ab0ac56084d43884f75e2059e5cf541
MWJOB <okkilltogo@gmail.com>
2022-12-20 13:38:00 +0900

    init

---
+++ src/main/java/com/example/seb41_pre_004/Seb41Pre004Application.java
@@ -0,0 +1,13 @@
+package com.example.seb41_pre_004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb41Pre004Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Seb41Pre004Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seb41_pre_004/Seb41Pre004ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seb41_pre_004;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb41Pre004ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
