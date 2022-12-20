445d17d1d77291ed5a8921ea3a9e46c1a217ab3a
gkgjswls <gkgjswls842@gmail.com>
2022-12-20 17:43:06 +0900

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components';
+import logo_github from './logo_github.svg'
+
+
+function App() {
+
+  return(<>
+  </>)
+}
+
+export default App;
