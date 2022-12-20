7b7ea324031a43d3b77ffa5f1920b2fc42c1c872
Joo614 <57wnguswn57@gmail.com>
2022-12-20 14:12:56 +0900

    test

---
+++ frontend/src/test/java/com/preproject/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preproject.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
