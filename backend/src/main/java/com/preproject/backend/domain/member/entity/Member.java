99f806d760070c592efdfa53c4e7c21fe9d6bb4f
MWJOB <okkilltogo@gmail.com>
2022-12-22 03:58:11 +0900

    .

--- backend/src/main/java/com/preproject/backend/domain/member/entity/Member.java
+++ backend/src/main/java/com/preproject/backend/domain/member/entity/Member.java
@@ -1,4 +1,65 @@
 package com.preproject.backend.domain.member.entity;
 
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.management.relation.Role;
+import javax.persistence.*;
+
+@Table(name = "MEMBER")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@AllArgsConstructor
+@Builder
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id; //primary Key
+
+    @Column(nullable = false, length = 30, unique = true)
+    private String username;//아이디
+
+    private String password;//비밀번호
+
+    @Column(nullable = false, length = 30)
+    private String name;//이름(실명)
+
+    @Column(nullable = false, length = 30)
+    private String nickName;//별명
+
+    @Column(nullable = false, length = 30)
+    private Integer age;//나이
+
+    @Enumerated(EnumType.STRING)
+    private Role role;//권한 -> USER, ADMIN
+
+
+
+
+
+    //== 정보 수정 ==//
+    public void updatePassword(PasswordEncoder passwordEncoder, String password){
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public void updateName(String name){
+        this.name = name;
+    }
+
+    public void updateNickName(String nickName){
+        this.nickName = nickName;
+    }
+
+    public void updateAge(int age){
+        this.age = age;
+    }
+
+    //== 패스워드 암호화 ==//
+    public void encodePassword(PasswordEncoder passwordEncoder){
+        this.password = passwordEncoder.encode(password);
+    }
+
 }
