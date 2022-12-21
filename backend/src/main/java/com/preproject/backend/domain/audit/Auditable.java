006eac82240220c30d04b2521e998837bd50887e
jackcmh1 <jackcmh1@gmail.com>
2022-12-21 14:56:30 +0900

    Auditable, application.yml 추가 (+ build.gradle 설정 추가)

---
+++ backend/src/main/java/com/preproject/backend/domain/audit/Auditable.java
@@ -0,0 +1,23 @@
+package com.preproject.backend.domain.audit;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Auditable {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
