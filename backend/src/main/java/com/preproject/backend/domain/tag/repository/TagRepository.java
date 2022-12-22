997fe68b84bd698d97fcd58182caebf308bfcfb7
jackcmh1 <jackcmh1@gmail.com>
2022-12-22 13:44:27 +0900

    Feat: Question,Tag 패키지 작성 시작(아직 진행중)

--- backend/src/main/java/com/preproject/backend/domain/tag/repository/TagRepository.java
+++ backend/src/main/java/com/preproject/backend/domain/tag/repository/TagRepository.java
@@ -1,4 +1,14 @@
 package com.preproject.backend.domain.tag.repository;
 
-public interface TagRepository {
+import com.preproject.backend.domain.tag.entity.TagEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<TagEntity, Integer> {
+    Optional<TagEntity> findByName(String name);
+
+    Page<TagEntity> findByNameIncluded(String name, Pageable pageable);
 }
--- backend/src/main/java/com/preproject/backend/domain/tag/service/TagService.java
+++ backend/src/main/java/com/preproject/backend/domain/tag/service/TagService.java
@@ -1,4 +1,40 @@
 package com.preproject.backend.domain.tag.service;
 
+import com.preproject.backend.domain.tag.entity.TagEntity;
+import com.preproject.backend.domain.tag.repository.TagRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@Slf4j
+@AllArgsConstructor
 public class TagService {
+    private TagRepository tagRepository;
+
+    public TagEntity createTag(String name){
+        String tagName = name.trim().toLowerCase();
+        Optional<TagEntity> optionalTagEntity = tagRepository.findByName(tagName);
+
+        TagEntity tag = new TagEntity();
+        tag.setName(tagName);
+
+        return tagRepository.save(tag);
+    }
+
+    public TagEntity updateTag(String name){
+        String tagName = name.trim().toUpperCase();
+        Optional<TagEntity> optionalTagEntity = tagRepository.findByName(tagName);
+
+        TagEntity tag = optionalTagEntity.get();
+        // TODO tag count + 1
+
+        return tagRepository.save(tag);
+    }
+
+    //TODO 페이지네이션
 }
