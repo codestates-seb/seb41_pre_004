e862c828ca622718352034a3a834afdf7fcf9402
junpotatoes <mukdol1127@naver.com>
2022-12-22 14:43:59 +0900

    feat: create QuestionHeader Component

--- frontend/src/components/QuestionHeader.js
+++ frontend/src/components/QuestionHeader.js
@@ -1,1 +1,26 @@
-//top question all question 구분없이 question 하나로 통일해 랜더링
+import React from 'react';
+import styled from 'styled-components';
+import QuestionHeaderBtn from './QuestionHeaderBtn';
+
+const QuestionContainer = styled.div`
+  border: 1px solid gray;
+  font-size: 27px;
+  width: 727px;
+  height: 100px;
+
+  display: flex;
+  justify-content: space-between;
+`;
+
+function QuestionHeader() {
+  return (
+    <>
+      <QuestionContainer>
+        <div>All question</div>
+        <QuestionHeaderBtn></QuestionHeaderBtn>
+      </QuestionContainer>
+    </>
+  );
+}
+
+export default QuestionHeader;
