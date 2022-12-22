a353d6fee2b6906ef9c8e0bd82cebb30c25cd1d4
junpotatoes <mukdol1127@naver.com>
2022-12-22 14:04:54 +0900

    feat: create QuestionHeaderBtn

---
+++ frontend/src/components/QuestionHeaderBtn.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+const QuestionBtn = styled.button`
+  width: 103px;
+  height: 37px;
+  border: 1px solid black;
+  background-color: #0a95ff;
+  color: white;
+  cursor: pointer;
+  text-align: center;
+  transition: 0.5s;
+  &:hover {
+    background: cornflowerblue;
+    color: white;
+    transition: 0.5s;
+`;
+
+function QuestionHeaderBtn() {
+  return (
+    <>
+      <QuestionBtn>Ask question</QuestionBtn>
+    </>
+  );
+}
+
+export default QuestionHeaderBtn;
