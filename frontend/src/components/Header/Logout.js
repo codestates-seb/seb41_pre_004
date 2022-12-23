195a1303da0be6a21f9dce59124b9b7f5feb6769
kimdong-hun <kimdonghun426@gmail.com>
2022-12-23 18:09:09 +0900

    chore: Mark up the not logged-in Header component

---
+++ frontend/src/components/Header/Logout.js
@@ -0,0 +1,77 @@
+import { Link } from 'react-router-dom';
+import { desktopLogo, mobileLogo } from '../../assets/headerIcons';
+import { Desktop, Tablet, Mobile } from '../Responsive';
+import Search from './Search';
+import MenuButton from './MenuButton';
+
+const Logout = () => {
+  return (
+    <>
+      <Desktop>
+        <Link to="/">
+          <strong>
+            <img src={desktopLogo} alt="stackoverflow logo" />
+          </strong>
+        </Link>
+        <a href="https://stackoverflow.co/" aria-label="Go to About page">
+          About
+        </a>
+        <button type="button" aria-label="Open the Products modal">
+          Products
+        </button>
+        <a
+          href="https://stackoverflow.co/teams/"
+          aria-label="Go to For Teams page"
+        >
+          For Teams
+        </a>
+        <Search />
+        <Link to="/login">
+          <strong>Log in</strong>
+        </Link>
+        <Link to="/signup">
+          <strong>Sign up</strong>
+        </Link>
+      </Desktop>
+
+      <Tablet>
+        <Link to="/">
+          <strong>
+            <img src={desktopLogo} alt="stackoverflow logo" />
+          </strong>
+        </Link>
+        <button type="button" aria-label="Open the Products modal">
+          Products
+        </button>
+        <Search />
+        <Link to="/login">
+          <strong>Log in</strong>
+        </Link>
+        <Link to="/signup">
+          <strong>Sign up</strong>
+        </Link>
+      </Tablet>
+
+      <Mobile>
+        <MenuButton />
+        <Link to="/">
+          <strong>
+            <img src={mobileLogo} alt="stackoverflow logo" />
+          </strong>
+        </Link>
+        <button type="button" aria-label="Open the Products modal">
+          Products
+        </button>
+        <Search />
+        <Link to="/login">
+          <strong>Log in</strong>
+        </Link>
+        <Link to="/signup">
+          <strong>Sign up</strong>
+        </Link>
+      </Mobile>
+    </>
+  );
+};
+
+export default Logout;
