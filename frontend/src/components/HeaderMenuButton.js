fa29577e46d688dade62a0a0fe307271938ebfcd
kimdong-hun <kimdonghun426@gmail.com>
2022-12-23 16:31:15 +0900

    chore: markup HeaderMenuButton component

---
+++ frontend/src/components/HeaderMenuButton.js
@@ -0,0 +1,19 @@
+import { menuIcon, cancleIcon } from '../assets/headerIcons';
+
+const HeaderMenuButton = ({ isOpen }) => {
+  return (
+    <>
+      {isOpen ? (
+        <button type="button" aria-label="Close the TabMenu">
+          <img src={cancleIcon} alt="" />
+        </button>
+      ) : (
+        <button type="button" aria-label="Open the TabMenu">
+          <img src={menuIcon} alt="" />
+        </button>
+      )}
+    </>
+  );
+};
+
+export default HeaderMenuButton;
