13dd51ef93f438d9100811c4f37b4358f5c05afe
kimdong-hun <kimdonghun426@gmail.com>
2022-12-23 15:01:23 +0900

    chore: modularize icons

---
+++ frontend/src/assets/headerIcons.js
@@ -0,0 +1,23 @@
+import desktopLogo from './icons/desktopLogo.svg';
+import mobileLogo from './icons/mobileLogo.svg';
+import searchIcon from './icons/searchIcon.svg';
+import inboxIcon from './icons/inboxIcon.svg';
+import trophyIcon from './icons/trophyIcon.svg';
+import helpIcon from './icons/helpIcon.svg';
+import snowIcon from './icons/snowIcon.svg';
+import listIcon from './icons/listIcon.svg';
+import menuIcon from './icons/menuIcon.svg';
+import cancleIcon from './icons/cancleIcon.svg';
+
+export {
+  desktopLogo,
+  mobileLogo,
+  searchIcon,
+  inboxIcon,
+  trophyIcon,
+  helpIcon,
+  snowIcon,
+  listIcon,
+  menuIcon,
+  cancleIcon,
+};
