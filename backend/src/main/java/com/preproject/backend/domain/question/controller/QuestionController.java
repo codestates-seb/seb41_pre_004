d1c9f380ae23cc238686706e5977a367b0ef0ad6
Joo614 <57wnguswn57@gmail.com>
2022-12-23 14:46:19 +0900

    Modify variable name

--- backend/src/main/java/com/preproject/backend/domain/question/controller/QuestionController.java
+++ backend/src/main/java/com/preproject/backend/domain/question/controller/QuestionController.java
@@ -19,49 +19,49 @@ import javax.validation.constraints.Positive;
 @Validated
 @AllArgsConstructor
 public class QuestionController {
-//    private final QuestionService questionService;
-//    private final QuestionMapper questionMapper;
-//    private final AnswerService answerService;
-//
-//
-//    //CREATE
-//    @PostMapping
-//    public ResponseEntity postQuestion(@Valid @RequestBody QuestionDto.Post post) {
-//        Question question = questionService.createQuestion( questionMapper.questionPostDtoToQuestion(post),
-//                                                            post.getTags());
-//        QuestionDto.ResponseCheck response = questionMapper.questionToResponseCheck(question);
-//
-//        return new ResponseEntity(response, HttpStatus.CREATED);
-//    }
-//
-//    //READ
-//    @GetMapping("/{question-id}")
-//    public ResponseEntity getQuestion(@Positive @PathVariable("question-id") int questionId) {
-//        Question question = questionService.readQuestion(questionId);
-//        //TODO Answer 패키지 구현 이후 이곳으로 돌아올 것
-//        // List<Answer> answers = answerService.getAnswer~~(question);
-//
-//    }
-//    //TODO 전체 질문 목록 read (필터, 페이지네이션, 태그 검색)
-//    // 클래스를 따로 만들어서 할 예정
-//
-//    //UPDATE
-//    @PatchMapping("/{question-id}")
-//    public ResponseEntity patchQuestion(@Positive @PathVariable("question-id") int questionId,
-//                                        @Valid @RequestBody QuestionDto.Patch patch) {
-//        Question question = questionService.updateQuestion( questionId,
-//                                                    questionMapper.questionPatchDtoToQuestion(patch),
-//                                                    patch.getTags());
-//
-//        QuestionDto.ResponseCheck response = questionMapper.questionToResponseCheck(question);
-//        return new ResponseEntity(response, HttpStatus.CREATED);
-//    }
-//
-//    //DELETE
-//    @DeleteMapping("/{question-id}")
-//    public ResponseEntity deleteQuestion(@Positive @PathVariable("question-id") int questionId) {
-//        questionService.deleteQuestion(questionId);
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//    }
+    private final QuestionService questionService;
+    private final QuestionMapper questionMapper;
+    private final AnswerService answerService;
+
+
+    //CREATE
+    @PostMapping
+    public ResponseEntity postQuestion(@Valid @RequestBody QuestionDto.Post post) {
+        Question question = questionService.createQuestion( questionMapper.questionPostDtoToQuestion(post),
+                                                            post.getTags());
+        QuestionDto.ResponseCheck response = questionMapper.questionToResponseCheck(question);
+
+        return new ResponseEntity(response, HttpStatus.CREATED);
+    }
+
+    //READ
+    @GetMapping("/{question-id}")
+    public ResponseEntity getQuestion(@Positive @PathVariable("question-id") int questionId) {
+        Question question = questionService.readQuestion(questionId);
+        //TODO Answer 패키지 구현 이후 이곳으로 돌아올 것
+        // List<Answer> answers = answerService.getAnswer~~(question);
+
+    }
+    //TODO 전체 질문 목록 read (필터, 페이지네이션, 태그 검색)
+    // 클래스를 따로 만들어서 할 예정
+
+    //UPDATE
+    @PatchMapping("/{question-id}")
+    public ResponseEntity patchQuestion(@Positive @PathVariable("question-id") int questionId,
+                                        @Valid @RequestBody QuestionDto.Patch patch) {
+        Question question = questionService.updateQuestion( questionId,
+                                                    questionMapper.questionPatchDtoToQuestion(patch),
+                                                    patch.getTags());
+
+        QuestionDto.ResponseCheck response = questionMapper.questionToResponseCheck(question);
+        return new ResponseEntity(response, HttpStatus.CREATED);
+    }
+
+    //DELETE
+    @DeleteMapping("/{question-id}")
+    public ResponseEntity deleteQuestion(@Positive @PathVariable("question-id") int questionId) {
+        questionService.deleteQuestion(questionId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
