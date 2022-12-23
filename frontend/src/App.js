311206d4b3873f8b1dfe78b043cf18dcaed83c20
kimdong-hun <kimdonghun426@gmail.com>
2022-12-23 14:54:45 +0900

    design: reset css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,34 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  a {
+    color: initial;
+    text-decoration: none;
+  }
+
+  input,
+  textarea {
+    padding: 0;
+    outline: none;
+    border: 1px solid #000000;
+    border-radius: 0;
+    box-shadow: none;
+  }
+
+  button {
+    padding: 0;
+    border: none;
+    border-radius: 0;
+    box-shadow: none;
+    background-color: inherit;
+    cursor: pointer;
+  }
 `;
 
 function App() {
