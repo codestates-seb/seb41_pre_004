c1a9d5cdb1aca5e642a915790978b94e83832d08
kimdong-hun <kimdonghun426@gmail.com>
2022-12-27 23:51:58 +0900

    design: style responsive

--- frontend/src/components/QuestionHeader.js
+++ frontend/src/components/QuestionHeader.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import QuestionHeaderBtn from './QuestionHeaderBtn';
+import { Mobile, Tablet, Desktop } from './Responsive';
 
 function QuestionHeader() {
   return (
     <>
-      <QuestionContainer>
-        <QuestionTitle>All Questions</QuestionTitle>
-        <QuestionHeaderBtn></QuestionHeaderBtn>
-      </QuestionContainer>
+      <Mobile>
+        <TabletContainer>
+          <MobileTitle>All Questions</MobileTitle>
+          <QuestionHeaderBtn></QuestionHeaderBtn>
+        </TabletContainer>
+      </Mobile>
+
+      <Tablet>
+        <TabletContainer>
+          <QuestionTitle>All Questions</QuestionTitle>
+          <QuestionHeaderBtn></QuestionHeaderBtn>
+        </TabletContainer>
+      </Tablet>
+
+      <Desktop>
+        <QuestionContainer>
+          <QuestionTitle>All Questions</QuestionTitle>
+          <QuestionHeaderBtn></QuestionHeaderBtn>
+        </QuestionContainer>
+      </Desktop>
     </>
   );
 }
@@ -20,10 +37,18 @@ const QuestionContainer = styled.div`
   padding: 32px 0 12px 24px;
 `;
 
+const TabletContainer = styled(QuestionContainer)`
+  padding: 24px 16px;
+`;
+
 const QuestionTitle = styled.h1`
   font-size: 27px;
   color: #232629;
   font-weight: 700;
+`;
+
+const MobileTitle = styled(QuestionTitle)`
+  font-size: 22px;
 `;
 
 export default QuestionHeader;
