e14959cd4196ec231f30631bf04e5f2c834df10b
junpotatoes <mukdol1127@naver.com>
2022-12-28 16:56:44 +0900

    feat: axios get question

--- frontend/src/pages/Question/index.js
+++ frontend/src/pages/Question/index.js
@@ -1,5 +1,5 @@
 //전체 질문목록 랜더링
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import QuestionHeader from '../../components/QuestionHeader';
 import Questionitem from '../../components/QuestionItem';
 import Navbar from '../../components/Navbar';
@@ -13,8 +13,23 @@ import {
   SideBlock,
 } from '../../styles/contentStyle';
 import { Desktop } from '../../components/Responsive';
+import QuestionList from '../../components/QuestionList';
+import axios from 'axios';
 
 function AllQuestion() {
+  const [homeData, setHomeData] = useState();
+
+  const fetchData = async () => {
+    const res = await axios(`http://localhost:3005/question`);
+    // const data = res;
+    console.log(res.data);
+    setHomeData(res.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <ContainerWrapper>
@@ -24,24 +39,8 @@ function AllQuestion() {
             <ContentBlock>
               <QuestionHeader></QuestionHeader>
               <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
-              <Questionitem></Questionitem>
+
+              {/* <QuestionList homeData={homeData} /> */}
             </ContentBlock>
             <Desktop>
               <SideBlock>
