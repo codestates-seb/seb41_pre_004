07555050c22393e2864b3606e59ce4022cdcba13
Joo614 <57wnguswn57@gmail.com>
2022-12-30 01:17:01 +0900

    feat: modify WebConfig

--- backend/src/main/java/com/preproject/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/preproject/backend/global/config/WebConfig.java
@@ -8,21 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 // CORS 에러 방지를 위함
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
-    }
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://ec2-3-36-23-23.ap-northeast-2.compute.amazonaws.com:8080"
-                                , "http://localhost:3000" , "http://127.0.0.1:3000" )
+                        .allowedOriginPatterns("*")
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
                         .exposedHeaders("Authorization", "RefreshToken");
