6d42f970b9d0d74e5beb99690caf8a5e3d7e05f0
MWJOB <okkilltogo@gmail.com>
2022-12-29 14:30:19 +0900

    test 2

--- backend/src/main/java/com/preproject/backend/BackendApplication.java
+++ backend/src/main/java/com/preproject/backend/BackendApplication.java
@@ -3,10 +3,18 @@ package com.preproject.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableJpaAuditing
+@RestController
 @SpringBootApplication
 public class BackendApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
