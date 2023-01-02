1bf987a7892818da7d34bda81afb8b15263f3db5
jackcmh1 <jackcmh1@gmail.com>
2023-01-02 14:54:10 +0900

    Refator: Question,Tag,Vote 패키지 정리

--- backend/src/main/java/com/preproject/backend/domain/tag/dto/TagDto.java
+++ backend/src/main/java/com/preproject/backend/domain/tag/dto/TagDto.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 public class TagDto {
 
-
     @Getter
     @Setter
     public static class Information{
--- backend/src/main/java/com/preproject/backend/domain/tag/mapper/TagMapper.java
+++ backend/src/main/java/com/preproject/backend/domain/tag/mapper/TagMapper.java
@@ -2,11 +2,9 @@ package com.preproject.backend.domain.tag.mapper;
 
 import com.preproject.backend.domain.tag.dto.TagDto;
 import com.preproject.backend.domain.tag.entity.TagEntity;
-import com.preproject.backend.global.page.PageInfo;
 import org.mapstruct.Mapper;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface TagMapper {
